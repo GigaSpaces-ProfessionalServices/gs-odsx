50e14e3a62b64ec58c34e14b59efd0514e9ffd0c
Tapan Prajapati <tapan.prajapati@niyantras.com>
2021-12-06 17:53:38 +0530

    ssl issue fixed

--- scripts/servers_northbound_preinstall.sh
+++ scripts/servers_northbound_preinstall.sh
@@ -20,25 +20,29 @@ mv $installation_path/nb.conf $targetDir/nb-infra/
 
 echo "copying ssl files to $targetDir/nb-infra/ssl"
 pfxFiles=$(ls install/nb/ssl/*.pfx 2> /dev/null | wc -l)
-if [  **"$pfxFiles" != "0"**  ]
+echo "pemFiles:"$pfxFiles
+if [[ $pfxFiles -gt 0 ]]
 then
     echo "Copying .pfx file"
     cp install/nb/ssl/*.pfx $targetDir/nb-infra/ssl/
 fi
 crtFiles=$(ls install/nb/ssl/*.crt 2> /dev/null | wc -l)
-if [  **"$crtFiles" != "0"**  ]
+echo "crtFiles:"$crtFiles
+if [[ $crtFiles -gt 0 ]]
 then
     echo "Copying .crt file"
     cp install/nb/ssl/*.crt $targetDir/nb-infra/ssl/
 fi
 pemFiles=$(ls install/nb/ssl/*.pem 2> /dev/null | wc -l)
-if [  **"$pemFiles" != "0"**  ]
+echo "pemFiles:"$pemFiles
+if [[  $pemFiles -gt 0 ]]
 then
     echo "Copying .pem file"
     cp install/nb/ssl/*.pem $targetDir/nb-infra/ssl/
 fi
 keyFiles=$(ls install/nb/ssl/*.key 2> /dev/null | wc -l)
-if [  **"$keyFiles" != "0"**  ]
+echo "keyFiles:"$keyFiles
+if [[ $keyFiles -gt 0 ]]
 then
     echo "Copying .key file"
     cp install/nb/ssl/*.key $targetDir/nb-infra/ssl/
