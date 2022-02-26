c1ed9224aee80f66074ed69acda9ca4b7534f3cd
Tapan Prajapati <tapan.prajapati@niyantras.com>
2022-02-26 13:00:25 +0530

    dev with security

---
+++ scripts/odsx_security_dev_manager_remove.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+# s6.py
+#!/usr/bin/python
+import os, subprocess, sys, argparse, platform
+from scripts.logManager import LogManager
+from utils.ods_ssh import executeRemoteShCommandAndGetOutput,connectExecuteSSH
+from utils.ods_app_config import readValuefromAppConfig
+from colorama import Fore
+from utils.ods_cluster_config import config_get_manager_listWithStatus,config_remove_manager_nodeByIP
+from scripts.spinner import Spinner
+
+verboseHandle = LogManager(os.path.basename(__file__))
+logger = verboseHandle.logger
+
+class bcolors:
+    OK = '\033[92m' #GREEN
+    WARNING = '\033[93m' #YELLOW
+    FAIL = '\033[91m' #RED
+    RESET = '\033[0m' #RESET COLOR
+
+def myCheckArg(args=None):
+    parser = argparse.ArgumentParser(description='Script to learn basic argparse')
+    parser.add_argument('m', nargs='?')
+    parser.add_argument('--host',
+                        help='host ip',
+                        required='True',
+                        default='localhost')
+    parser.add_argument('-u', '--user',
+                        help='user name',
+                        default='root')
+    parser.add_argument('-dryrun', '--dryrun',
+                        help='Dry run flag',
+                        default='false', action='store_true')
+    return verboseHandle.checkAndEnableVerbose(parser, sys.argv[1:])
+
+def handleException(e):
+    logger.info("handleException()")
+    trace = []
+    tb = e.__traceback__
+    while tb is not None:
+        trace.append({
+            "filename": tb.tb_frame.f_code.co_filename,
+            "name": tb.tb_frame.f_code.co_name,
+            "lineno": tb.tb_lineno
+        })
+        tb = tb.tb_next
+    logger.error(str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    }))
+    verboseHandle.printConsoleError((str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    })))
+
+
+def execute_scriptBuilder(host):
+    logger.info("execute_scriptBuilder(args)")
+    commandToExecute="scripts/servers_manager_remove.sh"
+
+    additionalParam = removeJava+' '+removeUnzip
+    logger.info("additionalParam : "+str(additionalParam))
+    with Spinner():
+        #outputShFile= executeRemoteShCommandAndGetOutput(host, 'root', additionalParam, commandToExecute)
+        outputShFile = connectExecuteSSH(host, user,commandToExecute,additionalParam)
+        print(outputShFile)
+        logger.info("Output : scripts/security_manager_remove.sh :"+str(outputShFile))
+        config_remove_manager_nodeByIP(host)
+        logger.debug(str(host)+" has been removed.")
+        verboseHandle.printConsoleInfo(str(host)+" has been removed.")
+
+
+def exitAndDisplay(isMenuDriven):
+    logger.info("exitAndDisplay(isMenuDriven)")
+    if(isMenuDriven=='m'):
+        logger.info("python3 scripts/odsx_security_manager_remove.py executed")
+        os.system('python3 scripts/odsx_security_manager_remove.py'+' '+isMenuDriven)
+
+if __name__ == '__main__':
+    logger.info("security - manager - Remove ")
+    verboseHandle.printConsoleWarning('Menu -> Security -> Dev -> Manager -> Remove')
+    args = []
+    menuDrivenFlag='m' # To differentiate between CLI and Menudriven Argument handling help section
+    args.append(sys.argv[0])
+
+    managerDict = config_get_manager_listWithStatus()
+
+    hostsConfig=''
+    hostsConfig = readValuefromAppConfig("app.manager.hosts")
+    logger.info("hostConfig:"+str(hostsConfig))
+    hostsConfig=hostsConfig.replace('"','')
+    if(len(str(hostsConfig))>0):
+        verboseHandle.printConsoleWarning("Current cluster configuration : ["+hostsConfig+"] ")
+    hostConfiguration = str(input(Fore.YELLOW+"press [1] if you want to remove individual server. \nPress [Enter] to remove current Configuration. \nPress [99] for exit.: "+Fore.RESET))
+    logger.info("hostConfiguration"+str(hostConfiguration))
+
+    try:
+        if len(sys.argv) > 1 and sys.argv[1] != menuDrivenFlag:
+            arguments = myCheckArg(sys.argv[1:])
+            global removeJava
+            global removeUnzip
+            if(arguments.dryrun==True):
+                current_os = platform.system().lower()
+                logger.debug("Current OS:"+str(current_os))
+                if current_os == "windows":
+                    parameter = "-n"
+                else:
+                    parameter = "-c"
+                exit_code = os.system(f"ping {parameter} 1 -w2 {arguments.host} > /dev/null 2>&1")
+                if(exit_code == 0):
+                    verboseHandle.printConsoleInfo("Connected to server with dryrun mode.!"+arguments.host)
+                    logger.debug("Connected to server with dryrun mode.!"+arguments.host)
+                else:
+                    verboseHandle.printConsoleInfo("Unable to connect to server."+arguments.host)
+                    logger.debug("Unable to connect to server."+arguments.host)
+                #output = subprocess.check_output("ssh -i ps-share.pem ec2-user@18.188.147.174 bash -s  < utils/java_check.sh | grep f89e7000 | awk '{print $1}'", shell=True)
+                try:
+                    exit_code = executeRemoteShCommandAndGetOutput(arguments.host, arguments.user, '', 'utils/java_check.sh')
+                    if(str(exit_code).__contains__('javac')):
+                        verboseHandle.printConsoleInfo("Java Installed.")
+                except Exception as e:
+                    verboseHandle.printConsoleInfo("Java not found.")
+                try:
+                    exit_code = executeRemoteShCommandAndGetOutput(arguments.host, arguments.user, '', 'utils/gs_check.sh')
+                    logger.info("exit_code"+str(exit_code))
+                    if(len(str(exit_code)) > 6):
+                        verboseHandle.printConsoleInfo("Gigaspace Installed.")
+                    else:
+                        verboseHandle.printConsoleInfo("Gigaspace not found.")
+                except Exception as e:
+                    verboseHandle.printConsoleInfo("Gigaspace not found.")
+                quit()
+
+            for arg in sys.argv[1:]:
+                args.append(arg)
+            logger.info('args : :'+str(args))
+            args = str(args)
+            logger.debug('Arguments :'+args)
+            execute_scriptBuilder(args)
+        elif(sys.argv[1]==menuDrivenFlag):
+            logger.info("Menudriven..")
+            args.append(menuDrivenFlag)
+            if(hostConfiguration=='1'):
+                optionMenu = str(input("Enter your host number to remove : "))
+                while(len(optionMenu)==0):
+                    optionMenu = str(input("Enter your host number to remove : "))
+                removeJava = str(input(Fore.YELLOW+"Do you want to remove Java ? (y/n) [n] :"))
+                if(len(str(removeJava))==0):
+                    removeJava='n'
+                removeUnzip = str(input(Fore.YELLOW+"Do you want to remove Unzip ? (y/n) [n] :"))
+                if(len(str(removeUnzip))==0):
+                    removeUnzip='n'
+                confirm = str(input(Fore.YELLOW+"Are you sure want to remove server ? (y/n) : "+Fore.RESET))
+                while(len(str(confirm))==0):
+                    confirm = str(input(Fore.YELLOW+"Are you sure want to remove server ? (y/n) : "+Fore.RESET))
+                logger.info("confirm :"+str(confirm))
+                if(confirm=='yes' or confirm=='y'):
+                    managerStart = managerDict.get(int(optionMenu))
+                    args.append('--host')
+                    args.append(str(managerStart.ip))
+                    #userConfig = readValuefromAppConfig("app.server.user")
+                    user = str(input("Enter your user [root]: "))
+                    if(len(str(user))==0):
+                        user="root"
+                    logger.info("app.server.user: "+str(user))
+                    #if(len(str(user))==0):
+                    #    user="ec2-user"
+                    args.append('-u')
+                    args.append(user)
+                    args.append('--id')
+                    args.append(str(managerStart.ip))
+                    args.append(removeJava)
+                    args.append(removeUnzip)
+                    execute_scriptBuilder(str(managerStart.ip))
+                elif(confirm =='no' or confirm=='n'):
+                    if(menuDrivenFlag=='m'):
+                        logger.info("menudriven")
+                        os.system('python3 scripts/odsx_security_manager_remove.py'+' '+menuDrivenFlag)
+            elif(hostConfiguration=='99'):
+                logger.info("99 - Exist stop")
+            else:
+                removeJava = str(input(Fore.YELLOW+"Do you want to remove Java ? (y/n) [n] :"))
+                if(len(str(removeJava))==0):
+                    removeJava='n'
+                removeUnzip = str(input(Fore.YELLOW+"Do you want to remove Unzip ? (y/n) [n] :"))
+                if(len(str(removeUnzip))==0):
+                    removeUnzip='n'
+
+                confirm = str(input(Fore.YELLOW+"Are you sure want to remove all servers ? [yes (y)] / [no (n)]"+Fore.RESET))
+                while(len(str(confirm))==0):
+                    confirm = str(input(Fore.YELLOW+"Are you sure want to remove all servers ? [yes (y)] / [no (n)]"+Fore.RESET))
+                logger.info("confirm :"+str(confirm))
+                if(confirm=='yes' or confirm=='y'):
+                    logger.info("Removing Cluster")
+                    #userConfig = readValuefromAppConfig("app.server.user")
+                    user = str(input("Enter your user [root]: "))
+                    if(len(str(user))==0):
+                        user='root'
+                    logger.info("app.server.user: "+str(user))
+                    #if(len(str(user))==0):
+                    #    user="ec2-user"
+                    hostsConfigArray = hostsConfig.split(",")
+                    for host in hostsConfigArray:
+                        logger.info("Removing host :"+str(host))
+                        verboseHandle.printConsoleInfo("Removing Host :"+str(host))
+                        args.append('--host')
+                        args.append(host)
+                        args.append('-u')
+                        args.append(user)
+                        args.append('--id')
+                        args.append(host)
+                        execute_scriptBuilder(host)
+                        args.remove("--host")
+                        args.remove(host)
+                        args.remove('-u')
+                        args.remove(user)
+                elif(confirm =='no' or confirm=='n'):
+                    if(menuDrivenFlag=='m'):
+                        logger.info("menudriven")
+                        os.system('python3 scripts/odsx_security_manager_remove.py'+' '+menuDrivenFlag)
+    except Exception as e:
+        logger.error("Invalid argument "+str(e))
+        verboseHandle.printConsoleError("Invalid argumentss")
+        handleException(e)
---
+++ scripts/odsx_security_dev_space_stop.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+# s6.py
+#!/usr/bin/python
+import os, subprocess, sys, argparse, platform
+from scripts.logManager import LogManager
+from utils.ods_ssh import executeRemoteShCommandAndGetOutput
+from utils.ods_cluster_config import config_get_space_list_with_status, config_get_space_hosts_list
+from colorama import Fore
+from utils.ods_app_config import readValuefromAppConfig
+
+verboseHandle = LogManager(os.path.basename(__file__))
+logger = verboseHandle.logger
+
+class bcolors:
+    OK = '\033[92m'  # GREEN
+    WARNING = '\033[93m'  # YELLOW
+    FAIL = '\033[91m'  # RED
+    RESET = '\033[0m'  # RESET COLOR
+
+def myCheckArg(args=None):
+    parser = argparse.ArgumentParser(description='Script to learn basic argparse')
+    parser.add_argument('m', nargs='?')
+    parser.add_argument('--host',
+                        help='host ip',
+                        required='True',
+                        default='localhost')
+    parser.add_argument('-u', '--user',
+                        help='user name',
+                        default='root')
+    parser.add_argument('-dryrun', '--dryrun',
+                        help='Dry run flag',
+                        default='false', action='store_true')
+    return verboseHandle.checkAndEnableVerbose(parser, sys.argv[1:])
+
+def exitAndDisplay(isMenuDriven):
+    if(isMenuDriven=='m'):
+        os.system('python3 scripts/odsx_security_space_stop.py'+' '+isMenuDriven)
+    else:
+        cliArgumentsStr=''
+        for arg in cliArguments:
+            cliArgumentsStr+=arg
+            cliArgumentsStr+=' '
+        os.system('python3 scripts/odsx_security_space_stop.py'+' '+cliArgumentsStr)
+
+if __name__ == '__main__':
+    logger.info("security - space - stop ")
+    verboseHandle.printConsoleWarning('Menu -> Security -> Dev -> Space -> Stop')
+    args = []
+    menuDrivenFlag='m' # To differentiate between CLI and Menudriven Argument handling help section
+    args.append(sys.argv[0])
+    try:
+        streamResumeStream=''
+        optionMainMenu=''
+        choice=''
+        cliArguments=''
+        isMenuDriven=''
+        managerRemove=''
+        # changed : 25-Aug hence systemctl always with root no need to ask
+        #userConfig = readValuefromAppConfig("app.server.user")
+        #logger.info("userConfig :"+str(userConfig))
+        #user = str(input("Enter your user ["+userConfig+"]: "))
+        #if(len(str(user))==0):
+        #    user=userConfig
+        user='root'
+        logger.info("user :"+str(user))
+        streamDict = config_get_space_list_with_status(user)
+        serverStartType = str(input(Fore.YELLOW+"press [1] if you want to stop individual server. \nPress [Enter] to stop all. \nPress [99] for exit.: "+Fore.RESET))
+        if(serverStartType=='1'):
+            optionMainMenu = int(input("Enter your host number to stop: "))
+            if(optionMainMenu != 99):
+                if len(streamDict) >= optionMainMenu:
+                    spaceStart = streamDict.get(optionMainMenu)
+                    choice = str(input(Fore.YELLOW+"Are you sure want to stop server ? [yes (y)] / [no (n)] / [cancel (c)] :"+Fore.RESET))
+                    while(len(str(choice))==0):
+                        choice = str(input(Fore.YELLOW+"Are you sure want to stop server ? [yes (y)] / [no (n)] / [cancel (c)] :"+Fore.RESET))
+                    logger.info("choice :"+str(choice))
+                    if(choice.casefold()=='no' or choice.casefold()=='n'):
+                        if(isMenuDriven=='m'):
+                            os.system('python3 scripts/odsx_security_space_stop.py'+' '+isMenuDriven)
+                        else:
+                            exitAndDisplay(isMenuDriven)
+                    elif(choice.casefold()=='yes' or choice.casefold()=='y'):
+                        if len(sys.argv) > 1 and sys.argv[1] != menuDrivenFlag:
+                            arguments = myCheckArg(sys.argv[1:])
+                            if(arguments.dryrun==True):
+                                current_os = platform.system().lower()
+                                logger.debug("Current OS:"+str(current_os))
+                                if current_os == "windows":
+                                    parameter = "-n"
+                                else:
+                                    parameter = "-c"
+                                exit_code = os.system(f"ping {parameter} 1 -w2 {arguments.host} > /dev/null 2>&1")
+                                if(exit_code == 0):
+                                    verboseHandle.printConsoleInfo("Connected to server with dryrun mode.!"+arguments.host)
+                                    logger.debug("Connected to server with dryrun mode.!"+arguments.host)
+                                else:
+                                    verboseHandle.printConsoleInfo("Unable to connect to server."+arguments.host)
+                                    logger.debug("Unable to connect to server."+arguments.host)
+                                try:
+                                    exit_code = executeRemoteShCommandAndGetOutput(arguments.host, arguments.user, '', 'utils/java_check.sh')
+                                    if(str(exit_code).__contains__('javac')):
+                                        verboseHandle.printConsoleInfo("Java Installed.")
+                                    else:
+                                        verboseHandle.printConsoleInfo("Java not found.")
+                                except Exception as e:
+                                    verboseHandle.printConsoleInfo("Java not found.")
+                                try:
+                                    exit_code = executeRemoteShCommandAndGetOutput(arguments.host, arguments.user, '', 'utils/gs_check.sh')
+                                    if(len(str(exit_code)) > 6):
+                                        verboseHandle.printConsoleInfo("Gigaspace Installed.")
+                                    else:
+                                        verboseHandle.printConsoleInfo("Gigaspace not found.")
+                                except Exception as e:
+                                    verboseHandle.printConsoleInfo("Gigaspace not found.")
+                                quit()
+
+                            for arg in sys.argv[1:]:
+                                args.append(arg)
+                            #print('install :',args)
+                            args = str(args)
+                            logger.debug('Arguments :'+args)
+                        elif(sys.argv[1]==menuDrivenFlag):
+                            args.append(menuDrivenFlag)
+                            args.append('--host')
+                            args.append(spaceStart.ip)
+                            args.append('-u')
+                            args.append(user)
+                        args = str(args)
+                        #print('args',args)
+                        #logger.info('Menu driven flag :'+menuDrivenFlag)
+                        logger.debug('Arguments :'+args)
+                        args =args.replace('[','').replace("'","").replace("]",'').replace(',','').strip()
+                        #print(args)
+                        os.system('python3 scripts/servers_manager_scriptbuilder.py '+args)
+                        #os.system('python3 scripts/remote_script_exec.py '+args)
+                else:
+                    verboseHandle.printConsoleError("please select valid option")
+                    optionMainMenu=''
+                    choice=''
+                    exitAndDisplay(isMenuDriven)
+            else :
+                print("")
+        elif(serverStartType =='99'):
+            logger.info("99 - Exist stop")
+        else:
+            confirm = str(input(Fore.YELLOW+"Are you sure want to stop all servers ? [yes (y)] / [no (n)]"+Fore.RESET))
+            while(len(str(confirm))==0):
+                confirm = str(input(Fore.YELLOW+"Are you sure want to stop all servers ? [yes (y)] / [no (n)]"+Fore.RESET))
+            logger.info("confirm :"+str(confirm))
+            if(confirm=='yes' or confirm=='y'):
+                spaceHosts = config_get_space_hosts_list()
+                for host in spaceHosts:
+                    args.append(menuDrivenFlag)
+                    args.append('--host')
+                    args.append(host)
+                    args.append('-u')
+                    args.append(user)
+                    argsString = str(args)
+                    logger.debug('Arguments :'+argsString)
+                    argsString =argsString.replace('[','').replace("'","").replace("]",'').replace(',','').strip()
+                    logger.info(argsString)
+                    os.system('python3 scripts/servers_manager_scriptbuilder.py '+argsString)
+                    args.remove(menuDrivenFlag)
+                    args.remove("--host")
+                    args.remove(host)
+                    args.remove('-u')
+                    args.remove(user)
+                    logger.info(args)
+            elif(confirm =='no' or confirm=='n'):
+                if(isMenuDriven=='m'):
+                    logger.info("menudriven")
+                    os.system('python3 scripts/odsx_security_space_stop.py'+' '+isMenuDriven)
+    except Exception as e:
+        logger.error("Invalid argument space stop :"+str(e))
+        verboseHandle.printConsoleError("Invalid argument")
