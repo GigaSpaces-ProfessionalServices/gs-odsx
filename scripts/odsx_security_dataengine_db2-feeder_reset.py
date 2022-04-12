9f48828c863ac37c13788b9a53575ab347e1a71e
Tapan Prajapati <tapan.prajapati@niyantras.com>
2022-04-12 18:32:43 +0530

    Fixed for feeder start stop drop table

---
+++ scripts/odsx_security_dataengine_db2-feeder_reset.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import os, time, requests,json, subprocess, glob,sqlite3
+from colorama import Fore
+from scripts.logManager import LogManager
+from utils.odsx_print_tabular_data import printTabular
+from utils.ods_cluster_config import config_get_space_hosts, config_get_manager_node
+from utils.ods_validation import getSpaceServerStatus
+from utils.ods_app_config import set_value_in_property_file,readValueByConfigObj
+from utils.odsx_db2feeder_utilities import getQueryStatusFromSqlLite
+
+verboseHandle = LogManager(os.path.basename(__file__))
+logger = verboseHandle.logger
+
+class bcolors:
+    OK = '[92m'  # GREEN
+    WARNING = '[93m'  # YELLOW
+    FAIL = '[91m'  # RED
+    RESET = '[0m'  # RESET COLOR
+
+class host_dictionary_obj(dict):
+    # __init__ function
+    def __init__(self):
+        self = dict()
+
+    # Function to add key:value
+    def add(self, key, value):
+        self[key] = value
+
+def handleException(e):
+    logger.info("handleException()")
+    trace = []
+    tb = e.__traceback__
+    while tb is not None:
+        trace.append({
+            "filename": tb.tb_frame.f_code.co_filename,
+            "name": tb.tb_frame.f_code.co_name,
+            "lineno": tb.tb_lineno
+        })
+        tb = tb.tb_next
+    logger.error(str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    }))
+    verboseHandle.printConsoleError((str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    })))
+
+
+def sqlLiteDeleteTableDb2Feeder():
+    logger.info("sqlLiteDeleteTableDb2Feeder() ")
+    try:
+        db_file = str(readValueByConfigObj("app.dataengine.db2-feeder.sqlite.dbfile")).replace('"','').replace(' ','')
+        cnx = sqlite3.connect(db_file)
+        logger.info("Db connection obtained."+str(cnx))
+        logger.info("DELETE FROM db2_host_port")
+        cnx.execute("DELETE FROM db2_host_port")
+        verboseHandle.printConsoleInfo("Table : db2_host_port reseted.")
+        cnx.commit()
+        cnx.close()
+    except Exception as e:
+        handleException(e)
+
+if __name__ == '__main__':
+    logger.info("odsx_security_dataengine_db2-feeder_reset")
+    verboseHandle.printConsoleWarning("Menu -> Security -> DataEngine -> DB2-Feeder -> Reset")
+    try:
+        sqlLiteDeleteTableDb2Feeder()
+    except Exception as e:
+        verboseHandle.printConsoleError("Eror in odsx_db2-feeder_reset : "+str(e))
+        handleException(e)
