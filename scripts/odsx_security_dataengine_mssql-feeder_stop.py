f87d5bd1509b5ab3a6045cbbfae9f77c7dafbddb
Tapan Prajapati <tapan.prajapati@niyantras.com>
2022-04-12 18:32:06 +0530

    Fixed for feeder start stop drop table

--- scripts/odsx_security_dataengine_mssql-feeder_stop.py
+++ scripts/odsx_security_dataengine_mssql-feeder_stop.py
@@ -164,13 +164,14 @@ def inputParam():
         for i in range (1,elements+1):
             proceedToStopMSSQLFeeder(str(i))
 
-def sqlLiteGetHostAndPortByFileName(shFileName):
-    logger.info("sqlLiteGetHostAndPortByFileName() shFile : "+str(shFileName))
+def sqlLiteGetHostAndPortByFileName(puName):
+    logger.info("sqlLiteGetHostAndPortByFileName() shFile : "+str(puName))
     try:
         db_file = str(readValueByConfigObj("app.dataengine.mssql-feeder.sqlite.dbfile")).replace('"','').replace(' ','')
         cnx = sqlite3.connect(db_file)
         logger.info("Db connection obtained."+str(cnx))
-        mycursor = cnx.execute("SELECT host,port FROM mssql_host_port where file like '%"+str(shFileName)+"%' ")
+        logger.info("SQL: SELECT host,port FROM mssql_host_port where feeder_name like '%"+str(puName)+"%' ")
+        mycursor = cnx.execute("SELECT host,port FROM mssql_host_port where feeder_name like '%"+str(puName)+"%' ")
         myresult = mycursor.fetchall()
         cnx.close()
         for row in myresult:
@@ -186,7 +187,7 @@ def proceedToStopMSSQLFeeder(fileNumberToStop):
     puName = gs_space_dictionary_obj.get(str(fileNumberToStop))
     verboseHandle.printConsoleInfo("puName :"+str(puName))
     shFileName = fileNamePuNameDict.get(str(puName))
-    hostAndPort = str(sqlLiteGetHostAndPortByFileName(shFileName)).split(',')
+    hostAndPort = str(sqlLiteGetHostAndPortByFileName(puName)).split(',')
     print("hostAndPort"+str(hostAndPort))
     host = str(hostAndPort[0])
     port = str(hostAndPort[1])
