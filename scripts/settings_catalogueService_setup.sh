3dd54720519d632dcc322c7387ec74ad438f23ff
himalisathavara <himali.sathavara@gigaspaces.com>
2022-04-25 13:54:31 +0530

    changes done to generate log file for catalogue service under gs-odsx/logs directory

--- scripts/settings_catalogueService_setup.sh
+++ scripts/settings_catalogueService_setup.sh
@@ -5,6 +5,7 @@ home_dir_sh=$(pwd)
 service_name='catalogue-service.service'
 serviceJarName='catalogue-service.jar'
 consul_host=$1
+log_location=$home_dir_sh/logs
 cp $home_dir_sh/systemServices/catalogue/$service_name /tmp/$service_name
 
 serviceJar=$home_dir_sh/systemServices/catalogue/$serviceJarName
@@ -12,7 +13,7 @@ serviceJar=$(readlink --canonicalize $serviceJar)
 
 sed -i 's,$serviceJar,'$serviceJar',g' /tmp/$service_name
 sed -i 's,$consul_host,'$consul_host',g' /tmp/$service_name
-
+sed -i 's,$log_location,'$log_location',g' /tmp/$service_name
 
 sudo mv -f /tmp/$service_name /etc/systemd/system/
 sudo systemctl daemon-reload
