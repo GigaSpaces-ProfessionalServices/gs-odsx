44b88b7a4bc1efc067cb632024bdeff20150a16a
Tapan Prajapati <tapan.prajapati@niyantras.com>
2022-04-27 07:43:20 +0530

    stateless changes

--- utils/ods_list.py
+++ utils/ods_list.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 # !/usr/bin/python
 
+import os
 from colorama import Fore
 from utils.ods_validation import getTelnetStatus
+from scripts.logManager import LogManager
+from utils.ods_ssh import executeRemoteCommandAndGetOutputValuePython36
+
+verboseHandle = LogManager(os.path.basename(__file__))
+logger = verboseHandle.logger
 
 class bcolors:
     OK = '\033[92m'  # GREEN
@@ -10,34 +16,61 @@ class bcolors:
     FAIL = '\033[91m'  # RED
     RESET = '\033[0m'  # RESET COLOR
 
+def isInstalledAndGetVersionGrafana(host):
+    logger.info("isInstalledAndGetVersion")
+    commandToExecute='ls /usr/lib/systemd/system/grafana*'
+    logger.info("commandToExecute :"+str(commandToExecute))
+    outputShFile = executeRemoteCommandAndGetOutputValuePython36(host, 'root', commandToExecute)
+    outputShFile=str(outputShFile).replace('\n','')
+    if len(str(outputShFile)) ==0:
+        commandToExecute='ls /etc/systemd/system/grafana*'
+        logger.info("commandToExecute :"+str(commandToExecute))
+        outputShFile = executeRemoteCommandAndGetOutputValuePython36(host, 'root', commandToExecute)
+        outputShFile=str(outputShFile).replace('\n','')
+    logger.info("outputShFile :"+str(outputShFile))
+    return str(outputShFile)
+
 def getGrafanaServerDetails(grafanaServers):
+    logger.info("getGrafanaServerDetails()")
     dataArray=[]
     for server in grafanaServers:
-        status = getTelnetStatus(server.ip,3000)
-        if(status=='ON'):
-            dataArray=[Fore.GREEN+server.ip+Fore.RESET,
-                       Fore.GREEN+server.name+Fore.RESET,
-                       Fore.GREEN+server.role+Fore.RESET,
-                       Fore.GREEN+status+Fore.RESET]
-        else:
-            dataArray=[Fore.GREEN+server.ip+Fore.RESET,
-                       Fore.GREEN+server.name+Fore.RESET,
-                       Fore.GREEN+server.role+Fore.RESET,
-                       Fore.RED+status+Fore.RESET]
+        installStatus='No'
+        host = str(os.getenv(server.ip))
+        status = getTelnetStatus(host,3000)
+        install = isInstalledAndGetVersionGrafana(str(host))
+        logger.info("install : "+str(install))
+        if(len(str(install))>0):
+            installStatus='Yes'
+        dataArray=[Fore.GREEN+host+Fore.RESET,
+                   Fore.GREEN+host+Fore.RESET,
+                   Fore.GREEN+server.role+Fore.RESET,
+                   Fore.GREEN+installStatus+Fore.RESET if(installStatus=='Yes') else Fore.RED+installStatus+Fore.RESET,
+                   Fore.GREEN+status+Fore.RESET if(status=='ON') else Fore.RED+status+Fore.RESET]
     return dataArray
 
+def isInstalledAndGetVersionInflux(host):
+    logger.info("isInstalledAndGetVersion")
+    commandToExecute='ls /etc/systemd/system/influx*'
+    logger.info("commandToExecute :"+str(commandToExecute))
+    outputShFile = executeRemoteCommandAndGetOutputValuePython36(host, 'root', commandToExecute)
+    outputShFile=str(outputShFile).replace('\n','')
+    logger.info("outputShFile :"+str(outputShFile))
+    return str(outputShFile)
+
 def getInfluxdbServerDetails(influxdbServers):
+    logger.info("getInfluxdbServerDetails()")
     dataArray=[]
     for server in influxdbServers:
-        status = getTelnetStatus(server.ip,8086)
-        if(status=='ON'):
-            dataArray=[Fore.GREEN+server.ip+Fore.RESET,
-                       Fore.GREEN+server.name+Fore.RESET,
-                       Fore.GREEN+server.role+Fore.RESET,
-                       Fore.GREEN+status+Fore.RESET]
-        else:
-            dataArray=[Fore.GREEN+server.ip+Fore.RESET,
-                       Fore.GREEN+server.name+Fore.RESET,
-                       Fore.GREEN+server.role+Fore.RESET,
-                       Fore.RED+status+Fore.RESET]
+        host = str(os.getenv(server.ip))
+        installStatus='No'
+        install = isInstalledAndGetVersionInflux(str(host))
+        logger.info("install : "+str(install))
+        if(len(str(install))>0):
+            installStatus='Yes'
+        status = getTelnetStatus(host,8086)
+        dataArray=[Fore.GREEN+host+Fore.RESET,
+                   Fore.GREEN+host+Fore.RESET,
+                   Fore.GREEN+server.role+Fore.RESET,
+                   Fore.GREEN+installStatus+Fore.RESET if(installStatus=='Yes') else Fore.RED+installStatus+Fore.RESET,
+                   Fore.GREEN+status+Fore.RESET if(status=='ON') else Fore.RED+status+Fore.RESET,]
     return dataArray
