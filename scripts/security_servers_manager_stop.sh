a5825222198a67c2089119997391f2c626be9485
Tapan Prajapati <tapan.prajapati@niyantras.com>
2022-05-04 15:28:55 +0530

    Fixed for individual start / stop

---
+++ scripts/security_servers_manager_stop.sh
@@ -0,0 +1,11 @@
+echo "Stopping InsightEdge"
+source setenv.sh
+#echo $JAVA_HOME
+#echo $PATH
+
+cd $GS_HOME
+#sudo -s
+#./bin/gs.sh host kill-agent --all
+#systemctl daemon-reload
+systemctl stop gsa.service
+
