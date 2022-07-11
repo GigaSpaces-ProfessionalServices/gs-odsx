fc722bf5f07dc27b1afd46eb41f56807c954f034
Tapan Prajapati <tapan.prajapati@niyantras.com>
2022-07-11 14:56:29 +0530

    apply ctrl c handle cli for manager start stop

--- scripts/odsx_servers_manager_start.py
+++ scripts/odsx_servers_manager_start.py
@@ -7,7 +7,7 @@ from utils.ods_ssh import executeRemoteShCommandAndGetOutput
 from utils.ods_app_config import readValuefromAppConfig
 from colorama import Fore
 from scripts.odsx_servers_manager_list import listFileFromDirectory
-from utils.ods_cluster_config import config_get_manager_listWithStatus
+from utils.ods_cluster_config import config_get_manager_listWithStatus, config_get_manager_node
 from scripts.odsx_servers_manager_install import getManagerHostFromEnv
 
 verboseHandle = LogManager(os.path.basename(__file__))
@@ -18,6 +18,28 @@ class bcolors:
     WARNING = '\033[93m' #YELLOW
     FAIL = '\033[91m' #RED
     RESET = '\033[0m' #RESET COLOR
+
+def handleException(e):
+    logger.info("handleException()")
+    trace = []
+    tb = e.__traceback__
+    while tb is not None:
+        trace.append({
+            "filename": tb.tb_frame.f_code.co_filename,
+            "name": tb.tb_frame.f_code.co_name,
+            "lineno": tb.tb_lineno
+        })
+        tb = tb.tb_next
+    logger.error(str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    }))
+    verboseHandle.printConsoleError((str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    })))
 
 def myCheckArg(args=None):
     parser = argparse.ArgumentParser(description='Script to learn basic argparse')
@@ -44,6 +66,19 @@ def execute_scriptBuilder(args):
     os.system('python3 scripts/servers_manager_scriptbuilder.py '+args)
     logger.debug('python3 scripts/servers_manager_scriptbuilder.py completed :')
 
+def exitAndDisplay(isMenuDriven):
+    logger.info("exitAndDisplay(isMenuDriven)")
+    if(isMenuDriven=='m'):
+        logger.info("exitAndDisplay(MenuDriven) ")
+        os.system('python3 scripts/odsx_servers_manager_start.py'+' '+isMenuDriven)
+    else:
+        cliArgumentsStr=''
+        for arg in cliArguments:
+            cliArgumentsStr+=arg
+            cliArgumentsStr+=' '
+        os.system('python3 scripts/odsx_servers_manager_start.py'+' '+cliArgumentsStr)
+
+
 if __name__ == '__main__':
     logger.info("servers - manager - start ")
     verboseHandle.printConsoleWarning('Menu -> Servers -> Manager -> Start')
@@ -53,124 +88,129 @@ if __name__ == '__main__':
     try:
         managerDict = config_get_manager_listWithStatus()
         hostsConfig=''
+        streamResumeStream=''
+        optionMainMenu=''
+        choice=''
+        cliArguments=''
+        isMenuDriven=''
+        managerRemove=''
+        user='root'
         #hostsConfig = readValuefromAppConfig("app.manager.hosts")
         hostsConfig = getManagerHostFromEnv()
         logger.info("hostConfig:"+str(hostsConfig))
         hostsConfig=hostsConfig.replace('"','')
         if(len(str(hostsConfig))>0):
             verboseHandle.printConsoleWarning("Current cluster configuration : ["+hostsConfig+"] ")
-        hostConfiguration = str(input(Fore.YELLOW+"press [1] if you want to start individual server. \nPress [Enter] to start current Configuration. \nPress [99] for exit.: "+Fore.RESET))
-        logger.info("hostConfiguration"+str(hostConfiguration))
+        serverStartType = str(input(Fore.YELLOW+"press [1] if you want to start individual server. \nPress [Enter] to start current Configuration. \nPress [99] for exit.: "+Fore.RESET))
+        if(serverStartType=='1'):
+            optionMainMenu = int(input("Enter your host number to start: "))
+            logger.info("Enter your host number to start:"+str(optionMainMenu))
+            if(optionMainMenu != 99):
+                if len(managerDict) >= optionMainMenu:
+                    spaceStart = managerDict.get(optionMainMenu)
+                    choice = str(input(Fore.YELLOW+"Are you sure want to start server ? [yes (y)] / [no (n)] / [cancel (c)] :"+Fore.RESET))
+                    while(len(str(choice))==0):
+                        choice = str(input(Fore.YELLOW+"Are you sure want to start server ? [yes (y)] / [no (n)] / [cancel (c)] :"+Fore.RESET))
+                    #print("coice start server:"+str(choice))
+                    logger.info("choice :"+str(choice))
+                    if(choice.casefold()=='no' or choice.casefold()=='n'):
+                        if(isMenuDriven=='m'):
+                            logger.info("menudriven")
+                            os.system('python3 scripts/odsx_servers_manager_start.py'+' '+isMenuDriven)
+                        else:
+                            exitAndDisplay(isMenuDriven)
+                    elif(choice.casefold()=='yes' or choice.casefold()=='y'):
+                        if len(sys.argv) > 1 and sys.argv[1] != menuDrivenFlag:
+                            arguments = myCheckArg(sys.argv[1:])
+                            if(arguments.dryrun==True):
+                                current_os = platform.system().lower()
+                                logger.debug("Current OS:"+str(current_os))
+                                if current_os == "windows":
+                                    parameter = "-n"
+                                else:
+                                    parameter = "-c"
+                                exit_code = os.system(f"ping {parameter} 1 -w2 {arguments.host} > /dev/null 2>&1")
+                                if(exit_code == 0):
+                                    verboseHandle.printConsoleInfo("Connected to server with dryrun mode.!"+arguments.host)
+                                    logger.debug("Connected to server with dryrun mode.!"+arguments.host)
+                                else:
+                                    verboseHandle.printConsoleInfo("Unable to connect to server."+arguments.host)
+                                    logger.debug("Unable to connect to server."+arguments.host)
+                                try:
+                                    exit_code = executeRemoteShCommandAndGetOutput(arguments.host, arguments.user, '', 'utils/java_check.sh')
+                                    if(str(exit_code).__contains__('javac')):
+                                        verboseHandle.printConsoleInfo("Java Installed.")
+                                    else:
+                                        verboseHandle.printConsoleInfo("Java not found.")
+                                except Exception as e:
+                                    verboseHandle.printConsoleInfo("Java not found.")
+                                try:
+                                    exit_code = executeRemoteShCommandAndGetOutput(arguments.host, arguments.user, '', 'utils/gs_check.sh')
+                                    if(len(str(exit_code)) > 6):
+                                        verboseHandle.printConsoleInfo("Gigaspace Installed.")
+                                    else:
+                                        verboseHandle.printConsoleInfo("Gigaspace not found.")
+                                except Exception as e:
+                                    verboseHandle.printConsoleInfo("Gigaspace not found.")
+                                quit()
 
-
-        if len(sys.argv) > 1 and sys.argv[1] != menuDrivenFlag:
-            arguments = myCheckArg(sys.argv[1:])
-            logger.info("arguments "+str(arguments))
-            if(arguments.dryrun==True):
-                current_os = platform.system().lower()
-                logger.debug("Current OS:"+str(current_os))
-                if current_os == "windows":
-                    parameter = "-n"
+                            for arg in sys.argv[1:]:
+                                args.append(arg)
+                            #print('install :',args)
+                            args = str(args)
+                            logger.debug('Arguments :'+args)
+                        elif(sys.argv[1]==menuDrivenFlag):
+                            args.append(menuDrivenFlag)
+                            args.append('--host')
+                            args.append(os.getenv(spaceStart.ip))
+                            args.append('-u')
+                            args.append(user)
+                        args = str(args)
+                        #print('args',args)
+                        #logger.info('Menu driven flag :'+menuDrivenFlag)
+                        logger.debug('Arguments :'+args)
+                        args =args.replace('[','').replace("'","").replace("]",'').replace(',','').strip()
+                        #print(args)
+                        os.system('python3 scripts/servers_manager_scriptbuilder.py '+args)
+                        #os.system('python3 scripts/remote_script_exec.py '+args)
                 else:
-                    parameter = "-c"
-                exit_code = os.system(f"ping {parameter} 1 -w2 {arguments.host} > /dev/null 2>&1")
-                if(exit_code == 0):
-                    verboseHandle.printConsoleInfo("Connected to server with dryrun mode.!"+arguments.host)
-                    logger.debug("Connected to server with dryrun mode.!"+arguments.host)
-                else:
-                    verboseHandle.printConsoleInfo("Unable to connect to server."+arguments.host)
-                    logger.debug("Unable to connect to server."+arguments.host)
-                try:
-                    exit_code = executeRemoteShCommandAndGetOutput(arguments.host, arguments.user, '', 'utils/java_check.sh')
-                    logger.info("exit_code"+str(exit_code))
-                    if(str(exit_code).__contains__('javac')):
-                        verboseHandle.printConsoleInfo("Java Installed.")
-                except Exception as e:
-                    logger.error("Java not found")
-                    verboseHandle.printConsoleInfo("Java not found.")
-                try:
-                    exit_code = executeRemoteShCommandAndGetOutput(arguments.host, arguments.user, '', 'utils/gs_check.sh')
-                    logger.info("exit_code"+str(exit_code))
-                    if(len(str(exit_code)) > 6):
-                        verboseHandle.printConsoleInfo("Gigaspace Installed.")
-                except Exception as e:
-                    logger.error("Gigaspace not found.")
-                    verboseHandle.printConsoleInfo("Gigaspace not found.")
-                quit()
-
-            for arg in sys.argv[1:]:
-                args.append(arg)
-            #print('install :',args)
-            args = str(args)
-            logger.debug('Arguments :'+args)
-            logger.info('Arguments :'+str(args))
-            execute_scriptBuilder(args)
-        elif(sys.argv[1]==menuDrivenFlag):
-            logger.info("Menudriven..")
-            args.append(menuDrivenFlag)
-            if(hostConfiguration=='1'):
-                optionMenu = str(input("Enter your host number to start : "))
-                while(len(optionMenu)==0):
-                    optionMenu = str(input("Enter your host number to start : "))
-                confirm = str(input(Fore.YELLOW+"Are you sure want to start server ? [yes (y)] / [no (n)] : "+Fore.RESET))
-                while(len(str(confirm))==0):
-                    confirm = str(input(Fore.YELLOW+"Are you sure want to start server ? [yes (y)] / [no (n)] : "+Fore.RESET))
-                logger.info("confirm :"+str(confirm))
-                if(confirm=='yes' or confirm=='y'):
-                    managerStart = managerDict.get(int(optionMenu))
+                    verboseHandle.printConsoleError("please select valid option")
+                    optionMainMenu=''
+                    choice=''
+                    exitAndDisplay(isMenuDriven)
+            else :
+                print("")
+        elif(serverStartType =='99'):
+            logger.info("99 - Exist start")
+        else:
+            confirm=''
+            confirm = str(input(Fore.YELLOW+"Are you sure want to start all servers ? [yes (y)] / [no (n)] : "+Fore.RESET))
+            while(len(str(confirm))==0):
+                confirm = str(input(Fore.YELLOW+"Are you sure want to start all servers ? [yes (y)] / [no (n)] : "+Fore.RESET))
+            logger.info("confirm :"+str(confirm))
+            if(confirm=='yes' or confirm=='y'):
+                spaceHosts = config_get_manager_node()#config_get_space_hosts_list()
+                for host in spaceHosts:
+                    args.append(menuDrivenFlag)
                     args.append('--host')
-                    args.append(str(os.getenv(managerStart.ip)))
-                    # changed : 25-Aug hence systemctl always with root no need to ask
-                    #userConfig = readValuefromAppConfig("app.server.user")
-                    #user = str(input("Enter your user ["+userConfig+"]: "))
-                    #if(len(str(user))==0):
-                    #    user=userConfig
-                    user='root'
-                    logger.info("app.server.user: "+str(user))
-                    #if(len(str(user))==0):
-                    #    user="ec2-user"
+                    args.append(os.getenv(host.ip))
                     args.append('-u')
                     args.append(user)
-                    execute_scriptBuilder(args)
-                elif(confirm =='no' or confirm=='n'):
-                    if(menuDrivenFlag=='m'):
-                        logger.info("menudriven")
-                        os.system('python3 scripts/odsx_servers_manager_start.py'+' '+menuDrivenFlag)
-            elif(hostConfiguration=='99'):
-                logger.info("99 - Exist stop")
-            else:
-                confirm = str(input(Fore.YELLOW+"Are you sure want to start all servers ? [yes (y)] / [no (n)] : "+Fore.RESET))
-                while(len(str(confirm))==0):
-                    confirm = str(input(Fore.YELLOW+"Are you sure want to start all servers ? [yes (y)] / [no (n)] : "+Fore.RESET))
-                logger.info("confirm :"+str(confirm))
-                if(confirm=='yes' or confirm=='y'):
-                    logger.info("Starting Cluster")
-                    # changed : 25-Aug hence systemctl always with root no need to ask
-                    #userConfig = readValuefromAppConfig("app.server.user")
-                    #user = str(input("Enter your user ["+userConfig+"]: "))
-                    #if(len(str(user))==0):
-                    #    user=userConfig
-                    user='root'
-                    logger.info("app.server.user: "+str(user))
-                    #if(len(str(user))==0):
-                    #    user="ec2-user"
-                    hostsConfigArray = hostsConfig.split(",")
-                    for host in hostsConfigArray:
-                        logger.info("Starting host :"+str(host))
-                        verboseHandle.printConsoleInfo("Starting Host :"+str(host))
-                        args.append('--host')
-                        args.append(host)
-                        args.append('-u')
-                        args.append(user)
-                        execute_scriptBuilder(args)
-                        args.remove("--host")
-                        args.remove(host)
-                        args.remove('-u')
-                        args.remove(user)
-                elif(confirm =='no' or confirm=='n'):
-                    if(menuDrivenFlag=='m'):
-                        logger.info("menudriven")
-                        os.system('python3 scripts/odsx_servers_manager_start.py'+' '+menuDrivenFlag)
+                    argsString = str(args)
+                    logger.debug('Arguments :'+argsString)
+                    logger.info(argsString)
+                    argsString =argsString.replace('[','').replace("'","").replace("]",'').replace(',','').strip()
+                    #print(argsString)
+                    os.system('python3 scripts/servers_manager_scriptbuilder.py '+argsString)
+                    args.remove(menuDrivenFlag)
+                    args.remove("--host")
+                    args.remove(os.getenv(host.ip))
+                    args.remove('-u')
+                    args.remove(user)
+                    logger.info(args)
+            elif(confirm =='no' or confirm=='n'):
+                if(isMenuDriven=='m'):
+                    logger.info("menudriven")
+                    os.system('python3 scripts/odsx_servers_space_start.py'+' '+isMenuDriven)
     except Exception as e:
-        logger.error("Invalid arguement "+str(e))
-        verboseHandle.printConsoleError("Invalid argument")
+        handleException(e)
