e430a7a89d4979935e53d50fc7f225d0e097f4b1
himalisathavara <himali.sathavara@gigaspaces.com>
2022-08-04 13:41:47 +0530

    added space graceful and force shutdown and reload feature

---
+++ scripts/odsx_servers_space_forceshutdown_shutdown.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+import argparse
+from calendar import c
+from logging import exception
+import os
+from shutil import ExecError
+import sys
+
+import pexpect
+from utils.ods_app_config import readValuefromAppConfig, set_value_in_property_file
+from utils.odsx_dataengine_utilities import getAllFeeders
+from utils.odsx_db2feeder_utilities import deleteDB2EntryFromSqlLite, deleteMSSqlEntryFromSqlLite
+from utils.odsx_print_tabular_data import printTabular
+from scripts.logManager import LogManager
+from utils.ods_cluster_config import config_get_manager_node, config_get_nb_list, config_get_space_hosts_list
+from colorama import Fore
+from utils.ods_validation import getSpaceServerStatus
+from scripts.spinner import Spinner
+from utils.ods_ssh import executeRemoteCommandAndGetOutput 
+import requests, json
+from utils.odsx_space_shutdown_reload_utilities import displayPus, getManagerHost, getProcessingUnitList, getServiceListFromConsul, printSuccessSummary, shutdownSpaceServers, undeployFeeders, undeployMicroservices, undeploySpace, validateBeforeShutdown, validateTierSpace
+
+verboseHandle = LogManager(os.path.basename(__file__))
+logger = verboseHandle.logger
+user='root'
+class bcolors:
+    OK = '\033[92m'  # GREEN
+    WARNING = '\033[93m'  # YELLOW
+    FAIL = '\033[91m'  # RED
+    RESET = '\033[0m'  # RESET COLOR
+
+def myCheckArg(args=None):
+    parser = argparse.ArgumentParser(description='Script to learn basic argparse')
+    parser.add_argument('m', nargs='?')
+    parser.add_argument('-dryrun', '--dryrun',
+                        help='Dry run flag',
+                        default='false', action='store_true')
+    return verboseHandle.checkAndEnableVerbose(parser, sys.argv[1:])
+
+def handleException(e):
+    logger.info("handleException()")
+    trace = []
+    tb = e.__traceback__
+    while tb is not None:
+        trace.append({
+            "filename": tb.tb_frame.f_code.co_filename,
+            "name": tb.tb_frame.f_code.co_name,
+            "lineno": tb.tb_lineno
+        })
+        tb = tb.tb_next
+    logger.error(str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    }))
+    verboseHandle.printConsoleError((str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    })))
+
+
+def shutdownServers():
+   
+    logger.info("shutdownServers() : start")
+
+    puList =[]
+    consulServiceList = []
+    spaceList = []    
+    validationMsg = ''
+    
+    spaceName = readValuefromAppConfig("app.spacejar.space.name")
+    tierSpace = readValuefromAppConfig("app.tieredstorage.pu.spacename")
+    spaceList.append(tierSpace)
+    managerHost = getManagerHost()
+    #print("managerHost=>"+str(managerHost))
+    puList = getProcessingUnitList(managerHost)
+
+    
+    for pu in puList:
+        space = str(pu['name'])
+        if space == spaceName or space == tierSpace:
+            spaceList.append(space)
+        
+    #try:
+    verboseHandle.printConsoleInfo("Validating .......")
+    validationMsg = validateBeforeShutdown(managerHost, puList,tierSpace)
+    if(len(str(validationMsg))>0):
+        displayPus(puList)
+        
+        verboseHandle.printConsoleError(validationMsg)
+        exit(0)
+    else:
+        verboseHandle.printConsoleInfo("Verifying primary partitions......")
+        tierSpaceValidation = validateTierSpace(managerHost, tierSpace)
+        if(len(str(tierSpaceValidation))>0):
+            verboseHandle.printConsoleError(tierSpaceValidation)
+            exit(0)
+
+        printSuccessSummary(puList)
+        confirmMsg = Fore.YELLOW + "Do you really want to shutdown ? (Yes(y)/No(n)):" + Fore.RESET
+        choice = str(input(confirmMsg))
+        while(len(choice) == 0):
+            choice = str(input(confirmMsg))
+
+        while(choice.casefold()!='yes' and choice.casefold()!='no' and choice.casefold()!='y' and choice.casefold()!='n'):
+         
+            verboseHandle.printConsoleError("Invalid input")
+            choice = str(input(confirmMsg))
+
+        if (choice.casefold() == 'no' or choice.casefold()=='n'):
+            logger.info("Exiting without performing shutdown")
+            exit(0)
+
+    consulServiceList = getServiceListFromConsul()
+  
+   
+    feederMsg = undeployFeeders()
+    verboseHandle.printConsoleInfo(feederMsg)
+
+    
+    puMsg = undeployMicroservices(managerHost, puList, consulServiceList)
+    verboseHandle.printConsoleInfo(puMsg)
+    
+
+    shutdownSpaceServers(user)
+    
+    logger.info("shutdownServers() : end")
+    #except Exception as e:
+    #    logger.error("error occurred in shutdownServers()")
+
+
+
+ 
+if __name__ == '__main__':
+    #args = []
+    #menuDrivenFlag = 'm'  # To differentiate between CLI and Menudriven Argument handling help section
+    #args.append(sys.argv[0])
+    #myCheckArg()
+    #with Spinner():
+    shutdownServers()
---
+++ utils/odsx_dataengine_utilities.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+
+import os, subprocess, sqlite3, json, requests
+from colorama import Fore
+from scripts.logManager import LogManager
+from utils.ods_app_config import readValueByConfigObj, readValuefromAppConfig
+from utils.ods_cluster_config import config_get_manager_node
+from utils.ods_ssh import executeRemoteCommandAndGetOutput
+from utils.ods_validation import getSpaceServerStatus
+from requests.auth import HTTPBasicAuth
+
+from utils.odsx_db2feeder_utilities import getMSSQLQueryStatusFromSqlLite, getPasswordByHost, getQueryStatusFromSqlLite, getUsernameByHost
+
+verboseHandle = LogManager(os.path.basename(__file__))
+logger = verboseHandle.logger
+
+class bcolors:
+    OK = '[92m'  # GREEN
+    WARNING = '[93m'  # YELLOW
+    FAIL = '[91m'  # RED
+    RESET = '[0m'  # RESET COLOR
+
+class host_dictionary_obj(dict):
+    # __init__ function
+    def __init__(self):
+        self = dict()
+
+    # Function to add key:value
+    def add(self, key, value):
+        self[key] = value
+
+def handleException(e):
+    logger.info("handleException()")
+    trace = []
+    tb = e.__traceback__
+    while tb is not None:
+        trace.append({
+            "filename": tb.tb_frame.f_code.co_filename,
+            "name": tb.tb_frame.f_code.co_name,
+            "lineno": tb.tb_lineno
+        })
+        tb = tb.tb_next
+    logger.error(str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    }))
+    verboseHandle.printConsoleError((str({
+        'type': type(e).__name__,
+        'message': str(e),
+        'trace': trace
+    })))
+    
+def getManagerHost(managerNodes):
+    managerHost=""
+    try:
+        logger.info("getManagerHost() : managerNodes :"+str(managerNodes))
+        for node in managerNodes:
+            status = getSpaceServerStatus(os.getenv(node.ip))
+            if(status=="ON"):
+                managerHost = os.getenv(node.ip)
+        return managerHost
+    except Exception as e:
+        handleException(e)
+
+def getAllFeeders():
+    logger.info("getAllFeeders() : start")
+
+    profile=str(readValuefromAppConfig("app.setup.profile"))
+    managerNodes = config_get_manager_node()
+    global gs_space_dictionary_obj
+    managerHost = getManagerHost(managerNodes)
+    if profile=='security':
+        appId = str(readValuefromAppConfig("app.space.security.appId")).replace('"','')
+        safeId = str(readValuefromAppConfig("app.space.security.safeId")).replace('"','')
+        objectId = str(readValuefromAppConfig("app.space.security.objectId")).replace('"','')
+        logger.info("appId : "+appId+" safeID : "+safeId+" objectID : "+objectId)
+        username = str(getUsernameByHost(managerHost,appId,safeId,objectId))
+        password = str(getPasswordByHost(managerHost,appId,safeId,objectId))
+
+    try:
+        response=""
+        logger.info("managerHost :"+str(managerHost))
+        if profile == 'security':
+            response = requests.get("http://"+str(managerHost)+":8090/v2/pus/",auth = HTTPBasicAuth(username, password))
+        else:
+            response = requests.get("http://"+str(managerHost)+":8090/v2/pus/")
+        logger.info("response status of host :"+str(managerHost)+" status :"+str(response.status_code)+" Content: "+str(response.content))
+        jsonArray = json.loads(response.text)
+        verboseHandle.printConsoleWarning("Resources on cluster:")
+        
+        gs_space_dictionary_obj = host_dictionary_obj()
+        logger.info("gs_space_dictionary_obj : "+str(gs_space_dictionary_obj))
+        counter=0
+        dataTable=[]
+        for data in jsonArray:
+            hostId=''
+            if profile == 'security':
+                response2 = requests.get("http://"+str(managerHost)+":8090/v2/pus/"+str(data["name"])+"/instances",auth = HTTPBasicAuth(username, password))
+            else:
+                response2 = requests.get("http://"+str(managerHost)+":8090/v2/pus/"+str(data["name"])+"/instances")
+            jsonArray2 = json.loads(response2.text)
+            queryStatus = str(getQueryStatusFromSqlLite(str(data["name"]))).replace('"','')
+            for data2 in jsonArray2:
+                hostId=data2["hostId"]
+            if(len(str(hostId))==0):
+                hostId="N/A"
+            if(str(data["name"]).__contains__('db2')):
+                dataArray = [Fore.GREEN+str(counter+1)+Fore.RESET,
+                             Fore.GREEN+data["name"]+Fore.RESET,
+                             Fore.GREEN+str(hostId)+Fore.RESET,
+                             Fore.GREEN+str(data["sla"]["zones"])+Fore.RESET,
+                             Fore.GREEN+str(queryStatus)+Fore.RESET,
+                             Fore.GREEN+data["status"]+Fore.RESET
+                             ]
+                gs_space_dictionary_obj.add(str(counter+1),str(data["name"]))
+                counter=counter+1
+                dataTable.append(dataArray)
+
+        # For Kafka - Consumer
+        
+            if(str(data["name"]).casefold().__contains__('adabasconsumer')):
+                dataArray = [Fore.GREEN+str(counter)+Fore.RESET,
+                             Fore.GREEN+data["name"]+Fore.RESET,
+                             Fore.GREEN+str(hostId)+Fore.RESET,
+                             Fore.GREEN+str(data["sla"]["zones"])+Fore.RESET,
+                             Fore.GREEN+str("-")+Fore.RESET,
+                             Fore.GREEN+data["status"]+Fore.RESET
+                             ]
+                gs_space_dictionary_obj.add(str(counter+1),str(data["name"]))
+                counter=counter+1
+                dataTable.append(dataArray)
+        # For MS-SQL-Feeder
+        
+            if(str(data["name"]).__contains__('mssql')):
+                dataArray = [Fore.GREEN+str(counter+1)+Fore.RESET,
+                             Fore.GREEN+data["name"]+Fore.RESET,
+                             Fore.GREEN+str(hostId)+Fore.RESET,
+                             Fore.GREEN+str(data["sla"]["zones"])+Fore.RESET,
+                             Fore.GREEN+str(queryStatus)+Fore.RESET,
+                             Fore.GREEN+data["status"]+Fore.RESET
+                             ]
+                gs_space_dictionary_obj.add(str(counter+1),str(data["name"]))
+                counter=counter+1
+                dataTable.append(dataArray)
+        
+        logger.info("getAllFeeders() : end")
+        return dataTable
+        
+    except Exception as e:
+        handleException(e)
