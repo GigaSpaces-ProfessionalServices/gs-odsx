adcd8b209aed2972e937c5d4da5954c49d5d40da
sushilmasukhani15 <sushil.mansukhani@gigaspaces.com>
2026-09-09 19:55:21 +0530

    RHEL Northbound changes

--- scripts/servers_northbound_install.sh
+++ scripts/servers_northbound_install.sh
@@ -15,7 +15,10 @@ sed -i '/export NB_HOME/d' .bashrc
 #echo "">>setenv.sh
 echo "$path">>.bashrc
 
-setsebool -P httpd_read_user_content 1
-chcon -Rt httpd_sys_rw_content_t /dbagigalogs/nginx/
+SELINUX_STATUS=$(getenforce)
+if [[ "$SELINUX_STATUS" == "Enforcing" ]]; then
+  setsebool -P httpd_read_user_content 1
+  chcon -Rt httpd_sys_rw_content_t /dbagigalogs/nginx/
+fi
 
 echo "Setting NB_HOME -Done!"
